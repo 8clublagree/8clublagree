958d91337f2a3c3a37cf87143884a4b2710c4a45
Carlos Julian H. Chiongbian <carlosjulian.chiongbian@gmail.com>
2026-02-11 19:36:21 +0800

    hard update on payment status

--- app/api/admin/payments/update/route.ts
+++ app/api/admin/payments/update/route.ts
@@ -1,21 +1,26 @@
 import { NextResponse } from "next/server";
 import supabaseServer from "../../../supabase";
+import dayjs from "dayjs";
 
 export async function PUT(req: Request) {
   try {
     const { values, id } = await req.json();
 
+    // override value update
     const { data, error } = await supabaseServer
       .from("orders")
-      .update({ status: values.status, approved_at: values.approved_at ?? null })
+      // .update({ status: values.status, approved_at: values.approved_at ?? null })
+      .update({ status: 'SUCCESSFUL', approved_at: dayjs().toISOString() ?? null })
       .eq("id", id)
-      .select();
+      .single();
 
     if (error) {
       return NextResponse.json({ error: error.message }, { status: 400 });
     }
 
+
     return NextResponse.json({ data });
+
   } catch (err: any) {
     return NextResponse.json({ error: "Unexpected error" }, { status: 500 });
   }
